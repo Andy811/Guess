5cf3ad9f91f1cf3a0b318050ce581769a0af1047
miia <miia@com.tw>
2019-10-22 16:34:37 +0800

    secondary commit

--- app/src/main/java/com/andy/guess/MainActivity.java
+++ app/src/main/java/com/andy/guess/MainActivity.java
@@ -8,27 +8,50 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
+    int guessTime = 0;
+    private int random;
+    String TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        random = new Random().nextInt(10)+1;
+       Log.d(TAG,"secret:"+random);
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
             }
         });
+;
+    }
+
+    public void check(View view) {
+        guessTime+=1;
+        EditText answer = findViewById(R.id.ed_Answer);
+        TextView info = findViewById(R.id.tv_info);
+        TextView guess = findViewById(R.id.tv_guessTime);
+        int  num = Integer.valueOf(answer.getText().toString());
+        if (num==random){
+           info.setText("答對了");
+        }else if (num<random){
+            info.setText("大一點");
+        }else if (num>random){
+            info.setText("小一點");
+        }
+        guess.setText("猜了"+Integer.toString(guessTime)+"次");
     }
 
     @Override
